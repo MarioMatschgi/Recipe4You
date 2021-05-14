a8215a52bef7dc3a9d9584c9bc814256a84678c5
Mario Elsnig <46848943+MarioMatschgi@users.noreply.github.com>
2021-05-15 00:06:11 +0200

    Fixed bugs, refactored, auto language 💬

--- src/app/services/localization.service.ts
+++ src/app/services/localization.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import * as data_en from '../../lang/english.json';
 import * as data_de from '../../lang/german.json';
@@ -7,9 +8,22 @@ import * as data_de from '../../lang/german.json';
 })
 export class LocalizationService {
   data: typeof data_en;
+  langs: { [lang: string]: typeof data_en } = {};
 
   constructor() {
-    this.data = (data_en as any).default;
-    this.data = (data_de as any).default;
+    let lang: string = window.navigator.language.substr(0, 2);
+    if (!environment.production) lang = 'de';
+
+    this.langs['en'] = this.getLangData(data_en);
+    this.langs['de'] = this.getLangData(data_de);
+
+    this.data = this.langs[lang] || this.langs['en'];
+  }
+
+  getLangData(data) {
+    return {
+      ...this.langs['en'],
+      ...(data as any).default,
+    };
   }
 }
