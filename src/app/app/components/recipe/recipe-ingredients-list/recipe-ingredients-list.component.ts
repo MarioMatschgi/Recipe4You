93ddec8df64c1b2b0d150fdf7c30b2fae8d6306a
Mario Elsnig <46848943+MarioMatschgi@users.noreply.github.com>
2021-06-26 12:27:37 +0200

    When adding ingredient focus last name field

--- src/app/app/components/recipe/recipe-ingredients-list/recipe-ingredients-list.component.ts
+++ src/app/app/components/recipe/recipe-ingredients-list/recipe-ingredients-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { LocalizationService } from 'src/app/libraries/util/services/localization.service';
 
 import { RecipeIngredientData } from 'src/app/app/model/recipe.model';
@@ -27,5 +35,11 @@ export class RecipeIngredientsListComponent implements OnInit {
   add_ingredient() {
     this.ingredients.push(new RecipeIngredientData());
     this.ingredientsChange.emit(this.ingredients);
+    setTimeout(() => {
+      const ing = document.getElementById(
+        'ing-' + (this.ingredients.length - 1)
+      );
+      console.log((ing.firstChild as HTMLInputElement).focus());
+    });
   }
 }
